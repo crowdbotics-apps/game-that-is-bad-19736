10cc5111c4ffe81e811836e2b0bfc2534d1f69e5
Crowdbotics <team@crowdbotics.com>
2020-08-22 21:36:32 +0000

    Initial commit.

---
+++ ios/game_that_is_bad_19736Tests/game_that_is_bad_19736Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface game_that_is_bad_19736 : XCTestCase
+
+@end
+
+@implementation game_that_is_bad_19736
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
